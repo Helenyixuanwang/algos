29eef347daa6294095607695bc93045dbe6703d3
Helen Wang <wangyixuan@msn.com>
2023-08-01 22:26:49 -0700

    review leet 217

--- leet_217_contains_duplicate.py
+++ leet_217_contains_duplicate.py
@@ -59,14 +59,14 @@ def containsDuplicate(nums):
         :rtype: bool
         """
         if len(set(Counter(nums).values())) == 1 and min(Counter(nums).values())==1:
-            # print(set(Counter(nums).values()))
+            print(set(Counter(nums).values()))
             return False
         else:
             return True
 
 
 # nums = [1,2,3,1]
-nums = [1,2,3,4]
+nums = [1,2,3,4,4]
 # nums = [0,4,5,0,3,6]
 # nums = [7,10,5,5,6,6,4,10,5,4,9,4,9,6,5,9,6,3,6,5,6,7,7,4,9,9,10,5,8,1,8,3,2,7,5,10,1,8,5,8,4,3,6,4,9,4,2,8,3,2,2,1,5,6,3,2,6,1,8,6,2,9,1,4,5,10,8,5,10,5,10,1,4,8,3,6,4,10,9,1,1,1,2,2,9,6,6,8,1,9,2,5,5,2,1,8,5,2,3,10]
 print(containsDuplicate(nums))
