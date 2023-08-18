895006d9edcea96c8f8abd29f61093b3d4e2ceea
Helen Wang <wangyixuan@msn.com>
2023-08-17 21:41:56 -0700

    update leet 1413, use a faster method, prefix sum

--- leet_1413_minValToGetPositive.py
+++ leet_1413_minValToGetPositive.py
@@ -1,5 +1,6 @@
 # 1413. Minimum Value to Get Positive Step by Step Sum
-# the code works,but not very fast, need improvement
+
+
 # Given an array of integers nums, you start with an initial positive value startValue.
 
 # In each iteration, you calculate the step by step sum of startValue plus elements in nums (from left to right).
@@ -9,37 +10,52 @@
 # Input: nums = [-3,2,-3,4,2]
 # Output: 5
 
+# a faster and more effecient method, I wrote it after I read the explanation from the platform
 def minInitial(nums):
-    if min(nums) >= 0:
-        return 1
-    
-    start = 0
-    for num in nums:
-        start += abs(num)
-    
-    
-    
-    while start > 0:
-        print("start is ",start)
-        prefix = [nums[0]+start]
-        for i in range(1, len(nums)):
-            prefix.append( prefix[-1] + nums[i])
-        print('prefix :',prefix)
-        if min(prefix)>1 and start > 1:
-            print("too big")
-            start -= 1
-            continue
-        break
-    return start
-
-# nums = [-3,2,-3,4,2]
-# nums = [1,-2,-3]
-# nums = [1,2]
-# nums = [2,3,5,-5,-1]
+    prefix = [0]
+    for i in range(0, len(nums)):
+        prefix.append( prefix[-1] + nums[i])
+    # find the min val from the prefix array, minVal + startValue = 1, so startValue = 1 - minVal
+    minVal = min(prefix)
+    return 1-minVal
 nums = [-3,2,-3,4,2]
 
 
 print(minInitial(nums))
+
+
+# the code works,but not very fast, need improvement
+# def minInitial(nums):
+#     if min(nums) >= 0:
+#         return 1
+    
+#     start = 0
+#     for num in nums:
+#         start += abs(num)
+    
+    
+    
+#     while start > 0:
+#         print("start is ",start)
+#         prefix = [nums[0]+start]
+#         for i in range(1, len(nums)):
+#             prefix.append( prefix[-1] + nums[i])
+#         print('prefix :',prefix)
+#         if min(prefix)>1 and start > 1:
+#             print("too big")
+#             start -= 1
+#             continue
+#         break
+#     return start
+
+# # nums = [-3,2,-3,4,2]
+# # nums = [1,-2,-3]
+# # nums = [1,2]
+# # nums = [2,3,5,-5,-1]
+# nums = [-3,2,-3,4,2]
+
+
+# print(minInitial(nums))
 
 
     
